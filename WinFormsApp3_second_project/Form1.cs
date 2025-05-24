465d57e2b6ed61d6f55d1776e6db7c75230e9868
TasneemAlmasri <tasneemmasri4@gmail.com>
2025-05-24 13:35:29 +0300

    squares instead of circles

--- WinFormsApp3_second_project/Form1.cs
+++ WinFormsApp3_second_project/Form1.cs
@@ -1,11 +1,13 @@
-using System.Diagnostics;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
-using Emgu.CV.Util;
-
 //using NAudio.Wave;
-using MathNet.Numerics;
+//using MathNet.Numerics;
+
+using Emgu.CV.Util;
+using System.Diagnostics;
+
+
 
 namespace WinFormsApp3_second_project
 {
@@ -17,12 +19,20 @@ namespace WinFormsApp3_second_project
         {
             InitializeComponent();
             //paths
-            string imgpath1 = "D:\\chrome stuff\\multi\\hand edited\\photo_2025-05-22_08-51-28.jpg";
+            string imgpath1 =
+            //"D:\\chrome stuff\\multi\\hand edited\\photo_2025-05-22_08-51-28.jpg";
             //"D:/chrome stuff/multi/hand edited/img1.png";
-            //string imgpath2 = "D:/chrome stuff/multi/hand edited/img1 edited.png";
-            string imgpath2 = "D:\\chrome stuff\\multi\\hand edited\\photo_2025-05-22_08-51-28me.jpg";
+
+            //@"D:\chrome stuff\multi\jeeda\1.jpg";
+            //@"D:\chrome stuff\multi\jeeda\2.jpg";
+            @"D:\chrome stuff\multi\jeeda\3.jpg";
+            string imgpath2 =
+            //string imgpath2 = "D:\\chrome stuff\\multi\\hand edited\\photo_2025-05-22_08-51-28me.jpg";
             //"D:/chrome stuff/multi/hand edited/img1 edited circles.png";
 
+            //@"D:\chrome stuff\multi\jeeda\1_e.jpg";
+            //@"D:\chrome stuff\multi\jeeda\2_e.jpg";
+            @"D:\chrome stuff\multi\jeeda\3_e.jpg";
 
 
             //read images
@@ -35,6 +45,8 @@ namespace WinFormsApp3_second_project
             Mat subimage = new Mat();
             CvInvoke.AbsDiff(img1, img2, subimage);
             //subimage.Save("D:/chrome stuff/multi/hand edited/subimg.png");
+            subimage.Save(@"D:\chrome stuff\multi\jeeda\sub.jpg");
+
 
 
 
@@ -43,32 +55,33 @@ namespace WinFormsApp3_second_project
             CvInvoke.CvtColor(subimage, grayscalimg, ColorConversion.Bgr2Gray);
 
             Mat binaryimg = new Mat();
-            CvInvoke.Threshold(grayscalimg, binaryimg, 30, 255, ThresholdType.Binary);
-            binaryimg.Save("D:/chrome stuff/multi/hand edited/tuqa.png");
+            CvInvoke.Threshold(grayscalimg, binaryimg, 20, 255, ThresholdType.Binary);
+            binaryimg.Save(@"D:\chrome stuff\multi\jeeda\binary.jpg");
 
 
 
             //finding all contours
             VectorOfVectorOfPoint allContours = new VectorOfVectorOfPoint(); //contour is a list of points, VectorOfVectorOfPoint is a list of countours
             CvInvoke.FindContours(binaryimg, allContours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple); //null means dont save the hirearchy anywhere//use external when you only care about the main blocks not what's inside them //chain..: contour point storage method
+            //Debug.WriteLine($"length issssssssssssssssss {allContours.Size}");
 
 
-
-            //////draw boxes
+            ////draw boxes
             //for (int i = 0; i < allContours.Size; i++)
             //{
-            //    Debug.WriteLine($"i is heeeeeeeeeeere :{i} ");
+            //    //Debug.WriteLine($"i is heeeeeeeeeeere :{i} ");
 
-            //    //Rectangle rect = CvInvoke.BoundingRectangle(allContours[i]);
-            //    //CvInvoke.Rectangle(img2, rect, new MCvScalar(0, 0, 0), 2); //color BGR, and thikness
+            //    Rectangle rect = CvInvoke.BoundingRectangle(allContours[i]);
+            //    CvInvoke.Rectangle(img2, rect, new MCvScalar(0,255,0), 2); //color BGR, and thikness
 
-            //    var circle = CvInvoke.MinEnclosingCircle(allContours[i]);
-            //    CvInvoke.Circle(img2, Point.Round(circle.Center), (int)(circle.Radius * 1.2), new MCvScalar(0, 0, 0), 2);//point is needed and it is pointF ,so we rounded to point
+            //    //var circle = CvInvoke.MinEnclosingCircle(allContours[i]);
+            //    //CvInvoke.Circle(img2, Point.Round(circle.Center), (int)(circle.Radius * 1.2), new MCvScalar(0, 0, 255), 2);//point is needed and it is pointF ,so we rounded to point
 
             //}
 
-            //CvInvoke.Imwrite("D:\\chrome stuff\\multi\\hand edited\\highlighted.png", img2);
-            //CvInvoke.Imwrite("D:\\chrome stuff\\multi\\hand edited\\highlightedCircle.png", img2);
+            //CvInvoke.Imwrite(@"D:\chrome stuff\multi\jeeda\sq.jpg", img2);
+            //CvInvoke.Imwrite(@"D:\chrome stuff\multi\jeeda\circles.jpg", img2);
+
 
 
 
@@ -90,7 +103,7 @@ namespace WinFormsApp3_second_project
             pictureBox2.MouseClick += box_click;
 
             //add to form
-            this.Controls.Add(pictureBox1); // this referes to Form1, Controls is the collection of UI elements like buttons lables .. placed on the form,Add(bla) adds bla to te collection so it shows when the form is shown
+            this.Controls.Add(pictureBox1); // this referes to Form1, controls is the collection of UI elements like buttons lables .. placed on the form,Add(bla) adds bla to te collection so it shows when the form is shown
             this.Controls.Add(pictureBox2);
 
             //resize whole form to fit both images
@@ -98,35 +111,32 @@ namespace WinFormsApp3_second_project
 
 
             //click handedling
-            void box_click(object sender, MouseEventArgs e)
+            void box_click(object sender, MouseEventArgs e) // sender is the object that triggered the event — in this case, one of your two PictureBox controls.
             {
                 PictureBox clickedBox = sender as PictureBox;
                 if (clickedBox != null)
                 {
                     Debug.WriteLine($"Clickedddddddddddd at ({e.X}, {e.Y}) in {clickedBox.Name}");
-
                     Point clickedPoint = e.Location;
 
                     for (int i = 0; i < allContours.Size; i++)
                     {
                         var contour = allContours[i];
+                        Rectangle rect = CvInvoke.BoundingRectangle(contour);
 
-                        double isInside = CvInvoke.PointPolygonTest(contour, new PointF(clickedPoint.X, clickedPoint.Y), false);
+                        int xpad = (int)(rect.Width * 0.15);
+                        int ypad = (int)(rect.Height * 0.15);
 
-                        if (isInside > 0)
+                        Rectangle largRec = new Rectangle(rect.X - xpad, rect.Y - ypad, rect.Width + 2 * xpad, rect.Height + 2 * ypad);
+                        largRec.Intersect(new Rectangle(0, 0, img2.Width, img2.Height));
+
+                        if (largRec.Contains(clickedPoint))
                         {
-                            var circle = CvInvoke.MinEnclosingCircle(contour);
-                            CvInvoke.Circle(img2, Point.Round(circle.Center), (int)(circle.Radius * 1.2), new MCvScalar(0, 0, 0), 2);
+                            CvInvoke.Rectangle(img2, largRec, new MCvScalar(0,255,0), 2 );
                             pictureBox2.Image = img2.ToBitmap();
-
                         }
-
-
-
                     }
                 }
-
-
             }
         }
     }
--- WinFormsApp3_second_project/obj/Debug/net8.0-windows/WinFormsApp3_second_project.AssemblyInfo.cs
+++ WinFormsApp3_second_project/obj/Debug/net8.0-windows/WinFormsApp3_second_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp3_second_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cdeb854eb21b360acd871bad0dce214370801e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp3_second_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp3_second_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
